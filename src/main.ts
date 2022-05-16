07b8a49dbc0bb123e6b23b456dc6a2d0039f4b42
thatpix3l <git@thatpix3l.com>
2022-05-16 11:21:46 -0400

    Implement VRM model loader

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
 import * as THREE from "three";
-import { MathUtils } from "three";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import { VRM } from '@pixiv/three-vrm'
 
 // Scene and camera
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const scene: THREE.Scene = new THREE.Scene();
+const camera: THREE.Camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 camera.position.z = 5;
 camera.position.y = 3;
 
-camera.rotation.y = MathUtils.degToRad(-30);
-camera.rotation.x = MathUtils.degToRad(-20);
+camera.rotation.y = THREE.MathUtils.degToRad(-20);
+camera.rotation.x = THREE.MathUtils.degToRad(-20);
 
 // Animation rendering loop
-var renderer = new THREE.WebGLRenderer( { alpha: true, antialias: true } );
+const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer( { alpha: true, antialias: true } );
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-function animate() {
+
+const animate = () => {
     requestAnimationFrame(animate);
 
     renderer.render(scene, camera);
@@ -29,11 +32,33 @@ const divisions = 10;
 const gridHelper = new THREE.GridHelper(size, divisions);
 scene.add(gridHelper);
 
-// Kilometers Morales
-const gltfLoader = new GLTFLoader();
-gltfLoader.load("/kilometers morales project.glb", (gltf) => {
-    const root = gltf.scene;
-    scene.add(root);
-});
+let characterModelBones: Map<String, THREE.Object3D> = new Map();
 
-console.log("lmao");
+// Async model loader
+const load_model = async (model_path: string) => {
+
+    // GLTF loader that VRM files are based off of
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load(model_path, async (gltf) => {
+
+        const root: THREE.Scene | THREE.Group = (await VRM.from(gltf)).scene;
+        console.log((await VRM.from(gltf)));
+
+        root.traverse((child) => {
+
+            if (child.type === "Bone") {
+                characterModelBones.set(child.name, child);
+            }
+
+        });
+
+        console.log(characterModelBones);
+
+        scene.add(root);
+    },
+    (progress) => console.log("Loading model...", 100.0 * ( progress.loaded / progress.total ), '%' ),
+    (error) => console.error(error));
+
+};
+
+load_model("/kilometers morales T-POSE.vrm");
