554797289469bf3fd7621ee4080a079ed5a9409d
thatpix3l <git@thatpix3l.com>
2022-08-03 13:07:36 -0400

    Update types for AppConfig

--- src/lib/ts/models/config.ts
+++ src/lib/ts/models/config.ts
@@ -5,11 +5,12 @@ export type Status = {
 }
 
 export type AppConfig = {
+    [key: string]: any
     vmc_listen: string
     fm3d_listen: string
     fm3d_device: string
     api_listen: string
-    model_update_frequency: string
+    model_update_frequency: number
     scene_home: string
     scene_file: string
     config_file: string
